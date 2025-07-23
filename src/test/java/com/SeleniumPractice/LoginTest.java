a5d867643baf392f19e5521d20eaf1c38855745d
manohar0548 <manoharlakkaraju@gmail.com>
2025-07-23 11:22:52 +0530

    Initial Commit for Login Page

---
+++ src/test/java/com/SeleniumPractice/LoginTest.java
@@ -0,0 +1,32 @@
+package com.SeleniumPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+	public static void main(String[] args) throws Exception {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://github.com/");
+		Thread.sleep(2000);
+		
+//		Clicking on Signin Link
+		WebElement signInButton = driver.findElement(By.linkText("Sign in"));
+		signInButton.click();		
+		Thread.sleep(2000);
+		
+//		Login Page
+		WebElement loginInputField = driver.findElement(By.id("login_field"));
+		loginInputField.sendKeys("manoharlakkaraju@gmail.com");
+		WebElement passwordField = driver.findElement(By.id("password"));
+		passwordField.sendKeys("Manohar@0548");
+		WebElement loginButton = driver.findElement(By.name("commit"));
+		loginButton.click();
+		Thread.sleep(2000);
+		driver.quit();
+
+	}
+
+}
